089c9a9ea4bd24865ea0f7d15b9197a966888410
Weston49 <weston.agreda@gmail.com>
2024-03-22 00:26:03 -0400

    3/22/2024

--- rebus.sh
+++ rebus.sh
@@ -5,10 +5,10 @@ fi
 r=$(curl -s "https://www.xwordinfo.com/Rebus" | grep data-date=\"$date\" | wc | awk '{print $1}')
 if [ $r -eq 0 ] ; then
   echo "No"
-  echo "<h1 style=\"text-align:center\">No<h1>" > index.html
+  echo "<h1 style=\"text-align:center\">No<h1>" > /home/wagreda/Documents/isTodaysNYTCrosswordRebus/index.html
 else
   echo "Yes"
-  echo "<h1 style=\"text-align:center\">Yes<h1>" > index.html
+  echo "<h1 style=\"text-align:center\">Yes<h1>" > /home/wagreda/Documents/isTodaysNYTCrosswordRebus/index.html
 fi
 git -C /home/wagreda/Documents/isTodaysNYTCrosswordRebus commit -a -m $date
 git -C /home/wagreda/Documents/isTodaysNYTCrosswordRebus push

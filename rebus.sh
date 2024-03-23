df376f4ad05fffcde52e00ec280d8e4eaea5c862
Weston49 <weston.agreda@gmail.com>
2024-03-23 02:20:51 -0400

    working again

--- rebus.sh
+++ rebus.sh
@@ -4,9 +4,15 @@ if [ $# -eq 1 ] ; then
 fi
 r=$(curl -s "https://www.xwordinfo.com/Rebus" | grep data-date=\"$date\" | wc | awk '{print $1}')
 if [ $r -eq 0 ] ; then
-  echo "No"
+  cat index.html | head -n 10 > tmp.txt
+  echo "<p>No</p>" >> tmp.txt
+  cat index.html | tail -n 3 >> tmp.txt
+  cat tmp.txt > index.html
 else
-  echo "Yes"
+  cat index.html | head -n 10 > tmp.txt
+  echo "<p>Yes</p>" >> tmp.txt
+  cat index.html | tail -n 3 >> tmp.txt
+  cat tmp.txt > index.html
 fi
 git -C /home/wagreda/Documents/isTodaysNYTCrosswordRebus commit -a -m $date
 git -C /home/wagreda/Documents/isTodaysNYTCrosswordRebus push

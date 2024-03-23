071d701758b96dd171f676b0b0b837c920cc0f4c
Weston49 <weston.agreda@gmail.com>
2024-03-23 02:28:38 -0400

    adds manual checker for personal use

---
+++ isRebus.sh
@@ -0,0 +1,12 @@
+date=$(date +%m | awk '{x=$0+0;print x}')"/"$(date +%d | awk '{x=$0+0;print x}')"/"$(date +%Y | awk '{x=$0+0;print x}')
+if [ $# -eq 1 ] ; then
+  date=$1
+fi
+r=$(curl -s "https://www.xwordinfo.com/Rebus" | grep data-date=\"$date\" | wc | awk '{print $1}')
+echo $date
+if [ $r -eq 0 ] ; then
+  echo "No"
+else
+  echo "Yes"
+fi
+
--- rebus.sh
+++ rebus.sh
@@ -1,7 +1,4 @@
 date=$(date +%m | awk '{x=$0+0;print x}')"/"$(date +%d | awk '{x=$0+0;print x}')"/"$(date +%Y | awk '{x=$0+0;print x}')
-if [ $# -eq 1 ] ; then
-  date=$1
-fi
 r=$(curl -s "https://www.xwordinfo.com/Rebus" | grep data-date=\"$date\" | wc | awk '{print $1}')
 if [ $r -eq 0 ] ; then
   cat index.html | head -n 10 > tmp.txt
